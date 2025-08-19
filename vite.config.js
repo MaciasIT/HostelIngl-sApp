e1426485096d42b61a618d1d611db703e4408bb6
michelmacias.it <michelmacias.it@gmail.com>
2025-08-19 21:46:18 +0200

    Update dependencies and configuration

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Añade esta línea
+  base: "/HostelInglesApp/",
   plugins: [react()],
   css: {
     postcss: {
